3d1a1e7a910140aa5f95f6f27ca3b75f92eb7cc3
luke <lhm9cb@umsystem.edu>
2024-05-15 18:22:45 -0500

    some more stuff

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -3,12 +3,17 @@ use bevy::prelude::*;
 use async_bevy_web::prelude::ABWConfigPlugin;
 use async_bevy_web::prelude::LeptosAppPlugin;
 
+//this main implementation for the states needs to be cleaned up in main file 
+mod states;
+use states::StatePlugin;
+
 fn main(){
     App::new()
         .insert_resource(SiteName("Bevy + Leptos".to_owned()))
         .add_systems(Startup, print_running)
         .add_plugins(ABWConfigPlugin::new(60.0))
         .add_plugins(LeptosAppPlugin::new(MyApp))
+        .add_plugins(StatePlugin)
         .run();
 }
 
--- generator/src/states.rs
+++ generator/src/states.rs
@@ -1,5 +1,6 @@
-use bevvy::prelude::*;
+use bevy::prelude::*;
 
+#[derive(Debug,Clone,Copy,Default,States,Hash, Eq, PartialEq )]
 pub enum MachineState {
     #[default]
     Idle,
@@ -14,7 +15,10 @@ pub struct StatePlugin;
 
 impl Plugin for StatePlugin {
     fn build(&self,app: &mut App) {
-        app.add_state::<MachineState>();
-
+        app.add_state::<MachineState>()
+            .add_systems(update,game_state_input_events);
     }
+}
+pub fn game_state_input_events(mut next_state:ResMut<NextState<MachineState>>,state:Res <State<Machinestate>>){
+    println!("State changed to: {}",next_state);
 }
